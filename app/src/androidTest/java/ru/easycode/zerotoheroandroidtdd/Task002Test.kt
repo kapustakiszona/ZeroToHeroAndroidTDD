79fef4aefeb987c6619f55554f4d4b9edda863f6
kapustakiszona <niahaibudze@gmail.com>
2023-11-02 15:32:40 +0100

    002/solved

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
@@ -1,8 +1,9 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -12,13 +13,14 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class Task002Test {
+class Task001Test {
 
     @get:Rule
     var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun test_add_id() {
-        onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+    fun test_change_text() {
+        onView(withText("Hello World!")).check(doesNotExist())
+        onView(withText("I am an Android Developer!")).check(matches(isDisplayed()))
     }
 }
