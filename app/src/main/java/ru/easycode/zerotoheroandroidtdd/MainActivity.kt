d7a93b8e19ec185aae46b15849b4ce152ccc9fdb
kapustakiszona <niahaibudze@gmail.com>
2023-11-02 18:08:28 +0100

    007/solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,32 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var text: TextView
+   lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.changeButton)
+
+        button.setOnClickListener {
+            text.text = "I am an Android Developer!"
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        text.text = savedInstanceState.getString("changed_text")
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("changed_text", text.text.toString())
     }
 }
