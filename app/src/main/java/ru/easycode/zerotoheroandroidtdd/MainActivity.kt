0a369bef73a8e91848bb44e956a0802232bd14d8
kapustakiszona <niahaibudze@gmail.com>
2023-11-03 21:29:44 +0100

    011/solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,38 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    lateinit var textView: TextView
+    var count = Count.Base(2)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.incrementButton)
+        textView = findViewById(R.id.countTextView)
+
+        button.setOnClickListener {
+            val result = count.increment(textView.text.toString())
+            textView.text = result
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("key", count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getSerializable("key") as Count.Base
+    }
+
+
+    //data class Count(var value: Int = 0) : Serializable
+
 }
