813f15cf449edec0fd349e66709bee9582df03a6
kapustakiszona <niahaibudze@gmail.com>
2023-11-21 13:28:54 +0100

    020/solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,26 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.actionButton)
+        editText = findViewById(R.id.inputEditText)
+
+        button.setOnClickListener {
+            val newText =  editText.text
+            textView.text = newText
+            editText.text.clear()
+        }
     }
 }
