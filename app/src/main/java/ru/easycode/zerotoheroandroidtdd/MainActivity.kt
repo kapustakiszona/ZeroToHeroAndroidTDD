3eec131860a416f4c92aab61f61cf24bfa93579c
kapustakiszona <niahaibudze@gmail.com>
2023-11-21 23:29:41 +0100

    021/solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,30 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+
+
+            inputEditText.addTextChangedListener{
+                actionButton.isEnabled = it?.length!! >= 3
+            }
+
+            actionButton.setOnClickListener {
+                val text = inputEditText.text
+                titleTextView.text = text
+                inputEditText.text?.clear()
+            }
+
+        }
     }
+
 }
